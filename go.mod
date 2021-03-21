da565e5d2b849fde6ddec43f46e87354f6add5b3
lw000 <373102227@qq.com>
2021-03-21 18:59:36 +0800

    viper

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module demo/goWeb
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/spf13/viper v1.7.1
+)
--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 )
 
 func main() {
+	{
+		viper.SetConfigName("config") // name of config file (without extension)
+		viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+		viper.AddConfigPath(".")      // optionally look for config in the working directory
+		err := viper.ReadInConfig()   // Find and read the config file
+		if err != nil {               // Handle errors reading the config file
+			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+				// Config file not found; ignore error if desired
+			} else {
+				// Config file was found but another error was produced
+			}
+			panic(fmt.Errorf("Fatal error config file: %s \n", err))
+		}
+		version := viper.GetInt32("version")
+		log.Println(version)
+	}
+
 	engine := gin.Default()
 
 	engine.Static("/res", "./res")
